0a074dd30fb9dbb076a40963ccbf2575bbfe2e69
Hardik Gujrathi <gujarathihardik29@gmail.com>
2025-04-06 17:23:43 +0530

    changed the recuireter job ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,18 +13,17 @@ import Login from "./pages/login";
 function App() {
   return (
     <>
-      <InitUser></InitUser>
+      {/* <InitUser></InitUser> */}
       <Routes>
-      
-      <Route path="/" element={<Home />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/recruiter-jobs" element={<RecJobs />} />
-      <Route path="/jobs" element={<Jobs />} />
-      <Route path="/browse" element={<Browse />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/jobdesc" element={<Jobdesc />} />
-    </Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/recruiter-jobs" element={<RecJobs />} />
+        <Route path="/jobs" element={<Jobs />} />
+        <Route path="/browse" element={<Browse />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/jobdesc" element={<Jobdesc />} />
+      </Routes>
     </>
   );
 }
--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,14 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function getProperTitle(title: string) {
+  const str = title.split("_");
+  for (let i = 0; i < str.length; i++) {
+    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+  }
+  return str.join(" ");
 }
